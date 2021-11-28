faee96ee040da86331d3b2782dd507186e655512
Ian Matthew Sobule <imsobule@gmail.com>
2021-11-28 13:12:29 -0600

    start adding tools ui

--- src/drawing/Canvas.tsx
+++ src/drawing/Canvas.tsx
@@ -8,26 +8,35 @@ export function Canvas() {
   const { paths } = useCanvasContext();
   return (
     <div
-      className="flex flex-col w-full h-full max-w-screen-sm max-h-full mx-auto overflow-hidden sm:h-auto"
-      role="presentation"
-      touch-action="pinch-zoom"
-      style={{ touchAction: "pinch-zoom" }}
+      style={{ zIndex: 1 }}
+      className="max-w-[500px] max-h-[500px] h-full w-full border bg-white border-black my-4 p-3"
     >
-      <div className="relative">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 500 500"
-          {...svgProps}
-          ref={svgProps.ref}
-        >
-          {paths?.length && (
-            <g id="artPaths">
-              <Paths paths={paths} />
-            </g>
-          )}
-        </svg>
+      <div className="w-full h-full bg-black text-white p-4">
+        <div className="w-full h-full bg-white text-black flex items-center justify-center text-2xl">
+          <div
+            className="flex flex-col w-full h-full max-w-screen-sm max-h-full mx-auto overflow-hidden sm:h-auto"
+            role="presentation"
+            touch-action="pinch-zoom"
+            style={{ touchAction: "pinch-zoom" }}
+          >
+            <div className="relative">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 500 500"
+                {...svgProps}
+                ref={svgProps.ref}
+              >
+                {paths?.length && (
+                  <g id="artPaths">
+                    <Paths paths={paths} />
+                  </g>
+                )}
+              </svg>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import type { NextPage } from "next";
 import { Canvas } from "../drawing/Canvas";
-import { ToolBar } from "../drawing/Toolbar";
 import classNames from "classnames";
-import { CanvasProvider, defaultState } from "../lib/svg-canvas";
+import React from "react";
+import * as Slider from "@radix-ui/react-slider";
+import {
+  CanvasProvider,
+  defaultState,
+  useCanvasContext,
+} from "../lib/svg-canvas";
 const Home: NextPage = () => {
+  const [activeToolMenu, setActiveToolMenu] = React.useState<string | null>(
+    null
+  );
   return (
     <div className="h-full min-h-screen flex flex-col items-center  font-display text-8xl font-bold uppercase py-20 max-w-screen-md mx-auto">
       <h1 className="mb-2">
@@ -14,27 +22,11 @@ const Home: NextPage = () => {
       <div className="flex flex-row space-x-2 w-full">
         <CanvasProvider config={{ ...defaultState, paths: [] }}>
           <div className="flex flex-row items-center justify-center w-full">
-            <div className="max-w-[500px] max-h-[500px] h-full w-full border border-black my-4 p-3">
-              <div className="w-full h-full bg-black text-white p-4">
-                <div className="w-full h-full bg-white text-black flex items-center justify-center text-2xl">
-                  <Canvas />
-                </div>
-              </div>
-            </div>
-            <div className="text-3xl flex  ml-4 h-[500px]">
-              {/* <div className="w-8 left-0 h-[500px] top-0 bg-red-500 mr-4"></div> */}
-              <div className="flex flex-col">
-                <div className="flex items-center space-x-5">
-                  <div>Color</div>
-                </div>
-                <div className="flex items-center space-x-5">
-                  <div>Brush Size</div>
-                </div>
-                <div>Undo</div>
-                <div>Clear</div>
-                <div>Save</div>
-              </div>
-            </div>
+            <Canvas />
+            <Tools
+              activeToolMenu={activeToolMenu}
+              setActiveToolMenu={setActiveToolMenu}
+            />
           </div>
         </CanvasProvider>
       </div>
@@ -42,13 +34,104 @@ const Home: NextPage = () => {
   );
 };
 
-function Tools() {
+function BrushSize() {
+  const { strokeWidth, dispatch } = useCanvasContext();
   return (
-    <div className="flex flex-row items-center justify-center w-full">
-      <div className="flex flex-col items-center justify-center">
-        {/* tool icon */}
+    <Slider.Root
+      className="w-full h-full flex flex-col items-center justify-center"
+      orientation="vertical"
+      max={40}
+      min={5}
+      value={[strokeWidth]}
+      onValueChange={(value) => {
+        dispatch({ type: "SET_STROKE_WIDTH", width: value[0] });
+      }}
+    >
+      <Slider.Track className="h-full w-2 bg-white border border-black relative">
+        <Slider.Range className="bg-black w-2 rounded-full  absolute" />
+      </Slider.Track>
+      <Slider.Thumb className="w-6 h-6 bg-white rounded-full block border-2 border-black" />
+    </Slider.Root>
+  );
+}
 
-        {/* tool name */}
+function ColorPicker() {
+  const { strokeColor, dispatch } = useCanvasContext();
+  const colors = ["red", "blue", "yellow", "black", "white", "green"];
+  return (
+    <div className="h-full transform -translate-x-2 flex flex-col items-center space-y-2">
+      {colors.map((color) => {
+        return (
+          <div
+            onClick={() => {
+              dispatch({
+                type: "SET_STROKE_COLOR",
+                color: color,
+              });
+            }}
+            className="h-8 w-8 cursor-pointer"
+            style={{
+              backgroundColor: color,
+              border: color === "white" ? "1px solid black" : "none",
+            }}
+          ></div>
+        );
+      })}
+    </div>
+  );
+}
+
+function ToolButton({ children, active, onClick }) {
+  return (
+    <button
+      className={classNames(
+        "flex items-center space-x-5 font-bold uppercase  ease-out",
+        active ? "bg-black text-white" : "bg-white text-black"
+      )}
+    >
+      <div onClick={onClick}>{children}</div>
+    </button>
+  );
+}
+function Tools({ activeToolMenu, setActiveToolMenu }) {
+  const brushActive = activeToolMenu === "brush";
+  return (
+    <div className={classNames("text-3xl flex h-[500px] relative ml-4")}>
+      <div
+        className={classNames(
+          "absolute h-full transform transition  ease-in-out duration-300 ",
+          activeToolMenu ? "translate-x-0 delay-75" : "-translate-x-56"
+        )}
+      >
+        {activeToolMenu === "brush" && <BrushSize />}
+        {activeToolMenu === "color" && <ColorPicker />}
+      </div>
+      <div
+        className={classNames(
+          "flex flex-col transform transition  ease-linear",
+          activeToolMenu ? "translate-x-8" : "translate-x-0 delay-75"
+        )}
+      >
+        <ToolButton
+          onClick={() =>
+            setActiveToolMenu(activeToolMenu !== "color" ? "color" : null)
+          }
+          active={activeToolMenu === "color"}
+        >
+          Color
+        </ToolButton>
+        <ToolButton
+          onClick={() =>
+            setActiveToolMenu(activeToolMenu !== "brush" ? "brush" : null)
+          }
+          active={activeToolMenu === "brush"}
+        >
+          Brush Size
+        </ToolButton>
+
+        <div>Undo</div>
+        <div>Clear</div>
+        <div>Save</div>
       </div>
     </div>
   );
